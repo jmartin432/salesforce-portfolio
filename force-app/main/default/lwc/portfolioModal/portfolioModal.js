425779b61bcce2c249f7ebb44098b7d6d7bce512
Justin <info@justinlmartin.com>
2022-11-16 17:21:33 -0800

    added spinner to portfolioModal

--- force-app/main/default/lwc/portfolioModal/portfolioModal.js
+++ force-app/main/default/lwc/portfolioModal/portfolioModal.js
@@ -5,8 +5,14 @@ export default class PortfolioModal extends LightningElement {
     @api imageSource
     @api award
 
+    imageLoading = true
+
     closeModal() {
         let evt = new Event('closemodal')
         this.dispatchEvent(evt)
+    }
+
+    imageLoaded() {
+        this.imageLoading = false
     }
 }
