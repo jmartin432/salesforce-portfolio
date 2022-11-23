c1a7fdc74e585e908ed63e8a3102f30ab9dcaaca
Justin <info@justinlmartin.com>
2022-11-22 19:50:40 -0800

    added accordion to body

--- force-app/main/default/lwc/portfolioBody/portfolioBody.js
+++ force-app/main/default/lwc/portfolioBody/portfolioBody.js
@@ -3,4 +3,12 @@ import { LightningElement, api } from 'lwc';
 export default class PortfolioBody extends LightningElement {
     @api portfolioId
     @api staticResourcePath
+
+    activeSections = []
+
+    handleSectionToggle(event) {
+        this.activeSections = event.detail.openSections
+    }
+
+    renderedCallback() {}
 }
